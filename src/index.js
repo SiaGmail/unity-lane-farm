6f1135f008f28988d85925efdd9199ad10861d25
SiaGmail <athana.sia.a@gmail.com>
2025-04-15 22:23:00 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // optional – remove this line if you don't have index.css
+import UnityFarms from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <UnityFarms />
+  </React.StrictMode>
+);
